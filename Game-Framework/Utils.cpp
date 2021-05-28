c97bb5cd12fa66e79dd7e6566cc3d68724eec885
Clive Leddy <clive.leddy@gmail.com>
2021-05-28 15:28:48 +0200

    Added comments to the class.

--- Game-Framework/Utils.cpp
+++ Game-Framework/Utils.cpp
@@ -1,9 +1,24 @@
+/**
+* Author: Clive Leddy
+* File: Utils.cpp
+* Date: 2021-05-20
+* Summary:
+* This is a utility class that contains no data. It contains methods that perform a task without saving data
+* or a state. There is no constructor or destructor.
+*/
 #include "Utils.h"
 
- int Utils::getInputInt(const char* msg, const char* errMsg){
+/// <summary>
+/// Get input entered from standard in as an integer. The entered integer must be a positive value or the
+/// user will be asked to re-enter a new value.
+/// </summary>
+/// <param name="msg">A message to the user requesting input as a const char*.</param>
+/// <param name="errMsg">An error message informing the user that the input was incorrect of type const char*.</param>
+/// <returns>The entered value from the user as type int.</returns>
+int Utils::getInputInt(const char* msg, const char* errMsg){
 	string input;
 	bool valid = false;
-	
+
 	input.clear();
 
 	while(!valid){
--- Game-Framework/Utils.h
+++ Game-Framework/Utils.h
@@ -1,3 +1,12 @@
+/**
+* Author: Clive Leddy
+* File: Utils.h
+* Date: 2021-05-20
+* Summary:
+* This is a utility class that contains no data. It contains methods that perform a task without saving data
+* or a state. There is no constructor or destructor.
+*/
+
 #ifndef UTILS_H
 #define UTILS_H
 
@@ -12,9 +21,16 @@ using std::cout;
 using std::endl;
 
 class Utils{
-private:
 
 public:
+
+	/// <summary>
+	/// Get input entered from standard in as an integer. The entered integer must be a positive value or the
+	/// user will be asked to re-enter a new value.
+	/// </summary>
+	/// <param name="msg">A message to the user requesting input as a const char*.</param>
+	/// <param name="errMsg">An error message informing the user that the input was incorrect of type const char*.</param>
+	/// <returns>The entered value from the user as type int.</returns>
 	static int getInputInt(const char* msg, const char* errMsg);
 };
 #endif // !UTILS_H
