e8a5b1e780dcd5e643368f83c7770f9a5e8fb423
Clive Leddy <clive.leddy@gmail.com>
2021-05-28 15:54:57 +0200

    Added comments to all methods and method signatures.

--- Game-Framework/ApplicationInstructions.cpp
+++ Game-Framework/ApplicationInstructions.cpp
@@ -1,5 +1,19 @@
+/*
+* Name: Clive Leddy
+* File: ApplicationInstructions.cpp
+* Date: 2021-05-15
+*
+* Summary:
+* Gather data from the user. Start by displaying a menu of choices and prompt for a choice. Thus set the
+* games bankroll and the number of rounds the game will be played. After a valid choice has been selected
+* it is then executed.
+*/
+
 #include "ApplicationInstructions.h"
 
+/// <summary>
+/// Class constructor. Initialises the class fields bankroll and rounds to 0.
+/// </summary>
 ApplicationInstructions::ApplicationInstructions() : bankroll(0), rounds(0){
 	menu = nullptr;
 
@@ -13,17 +27,26 @@ ApplicationInstructions::ApplicationInstructions() : bankroll(0), rounds(0){
 	initialize();
 }
 
+/// <summary>
+/// Class destructor.
+/// </summary>
 ApplicationInstructions::~ApplicationInstructions(){
 	applicationMenuItems.clear();
 	delete menu;
 }
 
+/// <summary>
+/// Initialise the class by creating and populating a menu.
+/// </summary>
 void ApplicationInstructions::initialize(){
 
 	createGameMenu();
 	setMenu();
 }
 
+/// <summary>
+/// Create a new menu.
+/// </summary>
 void ApplicationInstructions::createGameMenu(){
 
 	try{
@@ -36,6 +59,9 @@ void ApplicationInstructions::createGameMenu(){
 	}
 }
 
+/// <summary>
+/// Populate the menu.
+/// </summary>
 void ApplicationInstructions::setMenu(){
 
 	menu->setTitle("Roulette");
@@ -47,6 +73,11 @@ void ApplicationInstructions::setMenu(){
 	menu->addMenuItem(applicationMenuItems[END]);
 }
 
+/// <summary>
+/// Gather data from the user. Start by displaying a menu of choices and prompt for a choice. After a valid 
+/// choice has been selected it is then executed.
+/// </summary>
+/// <returns>1 end the game, -1 as for a new choice, 0 to continue as type int.</returns>
 int ApplicationInstructions::gatherData(){
 	int choice = 0;
 	bool ans = false;
@@ -59,33 +90,51 @@ int ApplicationInstructions::gatherData(){
 	choice = Utils::getInputInt(prompt.c_str(), errMsg);
 
 	switch(choice){
+
 		case BANKROLL:
+		
 			ans = setBankroll();
+
 			break;
 		case ROUNDS:
+			
 			ans = setNumberOfRounds();
+
 			break;
 		case PLAY_GAME:
+			
 			if(canPlayGame()){
+			
 				return 0;
+
 			} else{
 				cout << "Both the bankroll and number of rounds must be entered before a game can be played." << endl;
 			}
+
 			break;
 		case END:
+			
 			return 1;
+			
 			break;
 		default:
+
 			ans = false;
 			break;
 	}
+
 	if(!ans){
+
 		cerr << "The value you entered is not valid. Try again." << endl << endl;
 	}
 
 	return -1;
 }
 
+/// <summary>
+/// Check if the entered data is valid to play a game of Roulette.
+/// </summary>
+/// <returns>True if can play a game otherwise false.</returns>
 bool ApplicationInstructions::canPlayGame()const{
 	int lines = 15;
 
@@ -104,15 +153,26 @@ bool ApplicationInstructions::canPlayGame()const{
 	return true;
 }
 
+/// <summary>
+/// Display the menu with delimiters.
+/// </summary>
 void ApplicationInstructions::displayMenu(){
 
 	menu->displayMenu('-', '-');
 }
 
+/// <summary>
+/// Get the current bankroll.
+/// </summary>
+/// <returns>The bankroll as type int.</returns>
 int ApplicationInstructions::getBankroll()const{
 	return bankroll;
 }
 
+/// <summary>
+/// Set the bankroll by requesting a sum to be entered by the user.
+/// </summary>
+/// <returns>True if the entered sum is positive otherwise false.</returns>
 bool ApplicationInstructions::setBankroll(){
 	const char* msg = "Enter bankroll (example 123): ";
 	const char* errMsg = "\nIncorrect input! Try again.";
@@ -128,10 +188,19 @@ bool ApplicationInstructions::setBankroll(){
 	return false;
 }
 
+/// <summary>
+/// Get the number of rounds to play in this game.
+/// </summary>
+/// <returns>The number of rounds to play as type int.</returns>
 int ApplicationInstructions::getNumberOfRounds()const{
 	return rounds;
 }
 
+/// <summary>
+/// Set the number of rounds by requesting the number of rounds to play by the user.
+/// Note: There is no upper limit on how many rounds a game should have!
+/// </summary>
+/// <returns>True if the entered sum is positive otherwise false.</returns>
 bool ApplicationInstructions::setNumberOfRounds(){
 	const char *msg= "Number of rounds to play: ";
 	const char* errMsg = "\nUnrecognised number of rounds! Try again.";
--- Game-Framework/ApplicationInstructions.h
+++ Game-Framework/ApplicationInstructions.h
@@ -1,3 +1,13 @@
+/*
+* Name: Clive Leddy
+* File: ApplicationInstructions.h
+* Date: 2021-05-15
+*
+* Summary:
+* Gather data from the user. Start by displaying a menu of choices and prompt for a choice. Thus set the 
+* games bankroll and the number of rounds the game will be played. After a valid choice has been selected 
+* it is then executed.
+*/
 #ifndef APPLICATION_INSTRUCTIONS_H
 #define APPLICATION_INSTRUCTIONS_H
 
@@ -10,28 +20,88 @@ using std::cerr;
 #include <new>
 using std::bad_alloc;
 
+/// <summary>
+/// Gather data from the user. Start by displaying a menu of choices and prompt for a choice. Thus set the 
+/// games bankroll and the number of rounds the game will be played. After a valid choice has been selected 
+/// it is then executed.
+/// </summary>
 class ApplicationInstructions{
 
 private:
 	int bankroll;
 	int rounds;
 	Menu* menu;
+	//Manage the menu items before they are added to the menu.
 	vector<string> applicationMenuItems;
 	enum MenuEnum{ ERROR, BANKROLL, ROUNDS, PLAY_GAME, END };
 
+	/// <summary>
+	/// Initialise the class by creating and populating a menu.
+	/// </summary>
 	void initialize();
+	
+	/// <summary>
+	/// Populate the menu.
+	/// </summary>
 	void setMenu();
+	
+	/// <summary>
+	/// Create a new menu.
+	/// </summary>
 	void createGameMenu();
+	
+	/// <summary>
+	/// Display the menu with delimiters.
+	/// </summary>
 	void displayMenu();
+	
+	/// <summary>
+	/// Set the bankroll by requesting a sum to be entered by the user.
+	/// </summary>
+	/// <returns>True if the entered sum is positive otherwise false.</returns>
 	bool setBankroll();
+
+	/// <summary>
+	/// Set the number of rounds by requesting the number of rounds to play by the user.
+	/// Note: There is no upper limit on how many rounds a game should have!
+	/// </summary>
+	/// <returns>True if the entered sum is positive otherwise false.</returns>
 	bool setNumberOfRounds();
 public:
 	
+	/// <summary>
+	/// Class constructor. Initialises the class fields bankroll and rounds to 0.
+	/// </summary>
 	ApplicationInstructions();
+	
+	/// <summary>
+	/// Class destructor.
+	/// </summary>
 	~ApplicationInstructions();
+	
+	/// <summary>
+	/// Check if the entered data is valid to play a game of Roulette.
+	/// </summary>
+	/// <returns>True if can play a game otherwise false.</returns>
 	bool canPlayGame()const;
+	
+	/// <summary>
+	/// Gather data from the user. Start by displaying a menu of choices and prompt for a choice. After a valid 
+	/// choice has been selected it is then executed.
+	/// </summary>
+	/// <returns>1 end the game, -1 as for a new choice, 0 to continue as type int.</returns>
 	int gatherData();
+	
+	/// <summary>
+	/// Get the current bankroll.
+	/// </summary>
+	/// <returns>The bankroll as type int.</returns>
 	int getBankroll()const;
+	
+	/// <summary>
+	/// Get the number of rounds to play in this game.
+	/// </summary>
+	/// <returns>The number of rounds to play as type int.</returns>
 	int getNumberOfRounds()const;
 };
 
