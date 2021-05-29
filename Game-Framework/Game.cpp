b565991b239e622707a86544f29b592d67058816
Clive Leddy <clive.leddy@gmail.com>
2021-05-29 21:04:12 +0200

    Move some of the application logic to methods for easier understanding of the class.

--- Game-Framework/Game.cpp
+++ Game-Framework/Game.cpp
@@ -1,15 +1,23 @@
 
-#include "RouletteGAme.h"
+#include "RouletteGame.h"
 #include "RoulettePlayer.h"
 #include "ApplicationInstructions.h"
-
+#include "Wrapper.h"
+#include "CrapPlayer.h"
+#include "Menu.h"
 #include <iostream>
 using std::cout;
 using std::endl;
 
 ApplicationInstructions AppInc;
-RoulettePlayer* player;//(AppInc.getBankroll()); // Start with a player that has 1000 Kr
-RouletteGame* game;
+Menu* gameMenu;
+vector<string> gameMenuItems;
+IGame* game;
+IPlayer* player;
+
+const enum menuEnum{
+	ERROR, CRAPS, ROULETTE, EXIT
+};
 
 class App{
 public:
@@ -34,38 +42,131 @@ int App::run(){
 	return 0;
 }
 
+/// <summary>
+/// Create an instance of a menu.
+/// </summary>
+void createMenu(){
+	try{
+
+		gameMenu = new Menu();
+
+	} catch(bad_alloc ba){
+
+		cerr << "bad_alloc caught: " << ba.what() << endl;
+	}
+
+}
+
+/// <summary>
+/// Initialise the menu.
+/// </summary>
+void setupMenu(){
+	gameMenuItems = {"Incorrect choice!", "Crap's", "Roulette", "Exit"};
+
+	gameMenu->setTitle("Game App");
+	gameMenu->setPrompt("Enter your choice: ");
+	gameMenu->setMenuItemsTitle("In order to play a game, you must first select the number adjacent to the name of a game!");
+	gameMenu->addMenuItem(gameMenuItems[CRAPS]);
+	gameMenu->addMenuItem(gameMenuItems[ROULETTE]);
+	gameMenu->addMenuItem(gameMenuItems[EXIT]);
+}
+
+/// <summary>
+/// Initialise the Game.
+/// </summary>
+void initialise(){
+	createMenu();
+	setupMenu();
+}
+
+/// <summary>
+/// Select a game to play or exit.
+/// </summary>
+/// <returns>A number representing a specific game as type int.</returns>
+int selectGame(){
+	int ctr = -1;
+
+	do{
+		gameMenu->displayMenu('-', '-');
+		ctr = Utils::getInputInt(gameMenu->getPrompt().c_str(), "Incorrect choice!");
+
+		switch(ctr){
+			case CRAPS:
+			case ROULETTE:
+			case EXIT:
+				return ctr;
+				break;
+			default:
+				cout << gameMenuItems[ERROR] << endl;
+				continue;
+				break;
+		}
+	} while(true);
+
+	return ctr;
+}
+
+/// <summary>
+/// Select a game to play or exit the application.
+/// </summary>
+/// <returns>0 indicating exiting the application or 1 the game did not run as type int.</returns>
+int runGame(){
+	bool choice = true;
+	do{
+		if(AppInc.canPlayGame()){
+
+			switch(selectGame()){
+				case CRAPS:
+					game = new Wrapper();
+					player = new CrapPlayer(AppInc.getBankroll());
+					break;
+				case ROULETTE:
+					game = new RouletteGame();
+					player = new RoulettePlayer(AppInc.getBankroll());
+					break;
+				case EXIT:
+					choice = false;
+					break;
+				default:
+					cout << gameMenuItems[ERROR] << endl;
+					break;
+			}
+
+			if(choice){
+
+				App app(player, game);
+				app.run();
+			}
+		} else{
+
+			cout << "The game did not run!" << endl;
+			return 1;
+		}
+	} while(choice);
+
+	return 0;
+}
+
 int main(){
+
+	initialise();
 
 	do{
 		int ctr = AppInc.gatherData();
 
 		if(ctr == 1){
-			return 0;
+			return 0;//End the game.
 
-		} else if(ctr == -1){
+		} else if(ctr == -1){//Try again!
 			continue;
 
-		} else if(ctr == 0){
+		} else if(ctr == 0){//Play game.
+			return runGame();
 			break;
 		}
 
 	} while(true);
 
-	if(AppInc.canPlayGame()){
-
-
-		player = new RoulettePlayer(AppInc.getBankroll()); // Start with a player that has 1000 Kr
-		game = new RouletteGame();
-
-		App app(player, game);
-
-		return app.run();
-
-	} else{
-
-		cout << "The game did not run!" << endl;
-		return 1;
-	}
 
 	return 0;
 }
